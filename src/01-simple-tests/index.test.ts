3f816a825f5c81b9d420cf111cca0a44ab965538
pawel <pawelmakarewicz897@gmail.com>
2026-04-19 22:54:10 +0200

    feat: add first

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -3,7 +3,7 @@
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    // Write your test here 123
   });
 
   test('should subtract two numbers', () => {
