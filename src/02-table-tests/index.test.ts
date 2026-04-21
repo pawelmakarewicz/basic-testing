ae0a0de61a3f6f2c36c5e15857be571543688f9d
pawel <pawelmakarewicz897@gmail.com>
2026-04-21 22:07:45 +0200

    feat: enable tests for simpleCalculator with comprehensive test cases

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,46 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  // Add
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+
+  // Subtract
+  { a: 5, b: 2, action: Action.Subtract, expected: 3 },
+  { a: 2, b: 5, action: Action.Subtract, expected: -3 },
+
+  // Multiply
+  { a: 2, b: 3, action: Action.Multiply, expected: 6 },
+  { a: 4, b: 0, action: Action.Multiply, expected: 0 },
+
+  // Divide
+  { a: 6, b: 2, action: Action.Divide, expected: 3 },
+  { a: 5, b: 2, action: Action.Divide, expected: 2.5 },
+
+  // Exponentiate
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 5, b: 0, action: Action.Exponentiate, expected: 1 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  it.each(testCases)(
+    'should return $expected for $a $action $b',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
+
+  test('should return null for invalid action', () => {
+    expect(simpleCalculator({ a: 1, b: 2, action: 'invalid' })).toBeNull();
   });
-  // Consider to use Jest table tests API to test all cases above
+
+  test('should return null for invalid arguments', () => {
+    expect(simpleCalculator({ a: '1', b: 2, action: Action.Add })).toBeNull();
+
+    expect(
+      simpleCalculator({ a: 1, b: undefined, action: Action.Add }),
+    ).toBeNull();
+  });
 });
